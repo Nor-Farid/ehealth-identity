8a631b2e9526f04e5a717c98bdfb2542797ae859
Nor-Farid <m.norfaridnizam@gmail.com>
2026-06-19 01:15:39 +0800

    Update hardhat.config.js
    add automated gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,44 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("dotenv").config();
 
 module.exports = {
   solidity: {
     version: "0.8.20",
-    settings: { optimizer: { enabled: true, runs: 200 } },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   },
+
   networks: {
-    hardhat: { chainId: 31337 },
+    hardhat: {
+      chainId: 31337
+    },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : []
+    }
   },
+
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || "",
+    apiKey: process.env.ETHERSCAN_API_KEY || ""
+  },
+
+  gasReporter: {
+    enabled: true
   }
-  /*localhost: {
-      url: "http://127.0.0.1:5173",
-      chainId: 31337 // <-- Add this line inside the localhost network block too!
-    }*/
+
+  /*
+  localhost: {
+    url: "http://127.0.0.1:5173",
+    chainId: 31337
+  }
+  */
 };
